49e966e52b1ecc46c4bd1466b35025b5ba179e43
Caio Oliveira <parafrenteeadiante@gmail.com>
2022-02-06 00:02:06 -0300

    docs: Complementando a documentação do endpoint POST /users/new e setando o host para o heroku

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,11 +5,15 @@ const {
 const { tokenValidator } = require('../helpers/tokenHandler');
 
 module.exports = async (req, _res, next) => {
-  const token = req.headers.authorization;
+  const { authorization } = req.headers;
 
-  if (!token) return next(tokenNotFound);
-
+  if (!authorization) return next(tokenNotFound);
+  console.log('authorization');
+  console.log(authorization);
   try {
+    const [, token] = authorization.split(' ');
+    console.log('token');
+    console.log(token);
     const { id, email, role } = tokenValidator(token);
 
     req.user = { id, email, role };
